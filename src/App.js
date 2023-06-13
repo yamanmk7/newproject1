88aa65085f202af5b3dec01de0fae962c75d6fdd
yamanmk7 <112765539+yamanmk7@users.noreply.github.com>
2023-06-13 22:27:53 +0300

    aa

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import Img from './images/img';
+import styles from './styles/app.css'
 
 function App() {
+
+  const data = [
+    {name: 'yaman', grade: 100},
+    {name: 'ali', grade: 80},
+    {name: 'mohamed', grade: 70},
+    {name: 'ahmed', grade: 60},
+    {name: 'khaled', grade: 50},
+    {name: 'mohnd', grade: 100},
+    {name: 'amer', grade: 80},
+
+  ]
+
+  const Row = ({name , grade}) => {
+    return (
+      <tr className='rowInTable'>
+        <td>{name}</td>
+        <td>{grade}</td>
+      </tr>
+    )
+  }
+
+  const renderRows = () => {
+    return data.map((item) => {
+      return <Row name={item.name} grade={item.grade}/>
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <img src={Img.img2}  style={{width:200, marginLeft:1100}}/>
+      <img src={Img.img1}  style={{width:200, marginLeft:50, marginTop:-200}}/>
+      </div>
+       
+      <p className='' style={{fontSize:30,marginTop:40,marginLeft:1100 }}>: نتائج المشتركين</p>
+      <div >
+      <table className='table'>
+        <tr>
+          <th>name</th>
+          <th>grade</th>
+        </tr>
+        
+        {renderRows()}
+        
+      </table>
+
+      </div>
     </div>
   );
 }
---
+++ src/images/img.js
@@ -0,0 +1,8 @@
+
+const Img ={
+    img1: require('../images/codezone.jpg'),
+    img2: require('../images/img1.jpg'),
+}
+
+export default Img;
+
